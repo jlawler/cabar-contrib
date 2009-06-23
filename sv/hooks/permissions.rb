2f0b4c58d594dd17320baff48fb17e073ea5510d
Jeremy Lawler <jeremylawler@gmail.com>
2009-06-23 17:17:23 -0500

    only change permissions on existing files

--- sv/hooks/permissions.rb
+++ sv/hooks/permissions.rb
@@ -1,10 +1,9 @@
 me=StartHook.add_hook(:fix_permissions){|facet|
   STDERR.puts "FIX PERMISSIONS HOOK!"
   dir=facet.service_dir
-  File.chmod(0750, File.join(dir,'supervise'))
-  File.chmod(0770, File.join(dir,'supervise/control'))
-  File.chmod(0550, File.join(dir,'supervise/stat'))
   ['supervise','supervise/control','supervise/stat'].each do|f|f=File.join(dir,f)
+    next unless File.exists?(f)
+    File.chmod(0750, f) 
     new_uid,new_gid=nil,nil
     new_uid=Etc.getpwnam(facet.user).uid if facet.user
     new_gid=Etc.getgrnam(facet.group).gid if facet.group
