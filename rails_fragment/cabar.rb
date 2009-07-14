01bd1893cee47955e984c8075d329d2273c0e46f
Jeremy Lawler <jeremylawler@gmail.com>
2009-07-13 23:38:09 -0500

    rails fragment component

--- rails_fragment/cabar.rb
+++ rails_fragment/cabar.rb
@@ -1,18 +1,24 @@
-Cabar::Plugin.new do
+Cabar::Plugin.new(:name => 'rails_fragment') do
+  require 'cabar/facet/path'
+
   facet :'rails/controllers',
-      :env_var => :RAILS_CONTROLLER_PATH,
-      :std_path => :'lib/rails/controllers'
+      :env_var => :RAILS_CONTROLLERS_PATH,
+      :std_path => 'lib/rails/controllers'
+
   facet :'rails/models',
-      :env_var => :RAILS_MODEL_PATH,
-      :std_path => :'lib/rails/model'
+      :env_var => :RAILS_MODELS_PATH,
+      :std_path => 'lib/rails/models'
+
   facet :'rails/views',
       :env_var => :RAILS_VIEWS_PATH,
-      :std_path => :'lib/rails/views'
+      :std_path => 'lib/rails/views'
+
   facet :'rails/helpers',
       :env_var => :RAILS_HELPERS_PATH,
-      :std_path => :'lib/rails/helpers'
+      :std_path => 'lib/rails/helpers'
+
   facet :'rails/plugins',
       :env_var => :RAILS_PLUGINS_PATH,
-      :std_path => :'lib/rails/plugins'
-
+      :std_path => 'lib/rails/plugins'
 end
+
---
+++ rails_fragment/lib/ruby/rails_fragment.rb
@@ -0,0 +1,27 @@
+
+# Interface to rails_fragment rails/* Facets.
+module RailsFragment
+  EXTRA_RAILS_MODEL_PATHS=[*(ENV['RAILS_MODELS_PATHS'].split(':'))].compact
+  EXTRA_RAILS_CONTROLLER_PATHS=[*(ENV['RAILS_CONTROLLERS_PATHS'].split(':'))].compact
+
+  def self.configure_rails config
+    if defined? ::AUTOLOAD_PATHS
+      ::AUTOLOAD_PATHS << EXTRA_RAILS_MODEL_PATHS
+      ::AUTOLOAD_PATHS << EXTRA_RAILS_CONTROLLER_PATHS
+    end
+    config.controller_paths << EXTRA_RAILS_CONTROLLER_PATHS
+  end
+  def self.path name = nil
+    @@path ||=
+      begin
+        h = { }
+        [ :controllers, :models, :views, :helpers, :plugins ].each do | n |
+	  n_v = n.to_s.upcase
+	  h[n] = (ENV["RAILS_#{n_v}_PATH"] || '').split(':')
+	end
+	h
+      end
+    name ? @@path[name.to_sym] : @path
+  end
+end
+
